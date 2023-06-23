4e1acab813c69f9ed0713d3fc90b6336efc71097
Joi Misenti <joipolloi1@gmail.com>
2023-06-23 17:26:30 -0400

    starting problem 8

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -71,7 +71,18 @@ var score = 74
   Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
-//Code Here
+if(score >= 90) {
+  console.log('A');
+} else if(score < 90 && score >= 80) {
+  console.log('B');
+} else if(score < 80 && score >= 70) {
+  console.log('C');
+} else if(score < 70 && score >= 60) {
+    console.log('D');
+} else {
+    console.log('F');
+}
+
 
 
 ////////// Intermediate Problems //////////
@@ -86,7 +97,8 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
   Create a variable named 'someNum' and set it equal to the fifth value of the 'myFavoriteNumbers' array.
 */
 
-//Code Here
+let someNum = myFavoriteNumbers[4];
+console.log(someNum);
 
 
 ////////// PROBLEM 7 //////////
@@ -94,7 +106,11 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 // Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
-//Code Here
+if(myFavoriteNumbers.length < 7) {
+  console.log("There are not enough elements in this array")
+} else {
+  someNum = myFavoriteNumbers[6];
+}
 
 
 ////////// PROBLEM 8 //////////
